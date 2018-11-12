c851cd4b5e32fcdd2c17ca92c08aa70a3a885a3b
Lee Wiseman <leewiseman97@gmail.com>
2018-11-12 15:46:31 +0000

    Method wrappers for Account and Printable classes

---
+++ ATM/BackEnd/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Account : Printable
+    {
+        float balance;
+        int AccountNumber;
+
+        public void printReceipt(String receipt)
+        {
+        base.Print();
+        //this method prints receipt
+        }
+
+        public float getBalance()
+        {
+            return balance;
+        }
+        public int getAccountNumber()
+        {
+            return AccountNumber;
+        }
+        
+    }
+
